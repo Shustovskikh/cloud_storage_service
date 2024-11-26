26a2440b385c8d6840a9fe692e47503580e6dffd
Shustovskikh <python.shustovskikh@gmail.com>
2024-11-26 23:09:05 +0300

    Made and ran Migrations

--- backend/files/views.py
+++ backend/files/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import File
+from .serializers import FileSerializer
 
-# Create your views here.
+class FileViewSet(ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
